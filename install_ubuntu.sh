4500f8af62ad743f930e905925a897395410664b
Pedro Rosanes <prosanes@gmail.com>
2017-05-02 15:25:54 -0300

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,5 +1,5 @@
 # Basic development
-sudo apt-get install -y vim git tmux i3 i3blocks
+sudo apt-get install -y vim git tmux i3 i3blocks arandr
 mkdir -p ~/dev
 cd ~/dev
 if [ ! -d "config" ]; then
@@ -13,19 +13,23 @@ ln -s ~/dev/config/git_template/ ~/.git_template/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qa
 
+# i3
+mkdir ~/.i3
+ln -s ~/dev/config/i3_config ~/.i3/config
+
+# Others
+sudo apt-get install -y htop inotify-tools
+
 # Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 
 # RbEnv
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 source ~/.bashrc
-sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
 rbenv install 2.3.1
 rbenv local 2.3.1
-
-# Others
-sudo apt-get install -y htop inotify-tools
